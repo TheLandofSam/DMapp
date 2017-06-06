fcb249363a9399598e5df9a9d4589134e48a1d4b
Darryl Kilzer <darrylkilzer@gmail.com>
2017-06-06 17:09:51 -0600

    broken models please fix krhyer

--- server/models/player.js
+++ server/models/player.js
@@ -8,7 +8,7 @@ var schema = new mongoose.Schema({
 	description: { type: String },
 	created: { type: Number, default: Date.now() },
 	// Relations
-	campaignId: { type: ObjectId, ref: models.campaign.name, required: true},
+	campaignId: { type: ObjectId, ref: models.campaign.name, required: true}
 });
 
 module.exports = mongoose.model(models.player.name, schema);
