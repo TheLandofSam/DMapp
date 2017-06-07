aa5d18281ed9611f62e765ce69cde92ff5fcde44
Vail Algatt <vail.algatt@gmail.com>
2017-06-07 14:35:18 -0600

    trying again

---
+++ www/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Boards from 'components/Boards'
+import Board from 'components/Board'
+import Login from 'components/Login'
+import Register from 'components/Register'
+
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/boards',
+      name: 'Boards',
+      component: Boards
+    },{
+      path: '/boards/:id',
+      name: 'Board',
+      component: Board
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    }
+  ]
+})
