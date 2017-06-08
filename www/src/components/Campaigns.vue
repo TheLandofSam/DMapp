97d9ca1f0e22ca6198162d158266d8f83d5613d2
Jason Tidwell <tidwell2726@ymail.com>
2017-06-08 15:15:21 -0600

    Added campaigns and Awesome Background

--- www/src/components/Campaigns.vue
+++ www/src/components/Campaigns.vue
@@ -1,16 +1,24 @@
 <template>
-  <div class="well">
-    <button @click="createCampaign">Add Campaign</button>
-    <ul>
-      <li v-for="campaign in campaigns">
-        <router-link :to="'/campaigns/'+campaign._id">
-          <div class="well">
-            {{campaign.name}}
-            <button class="fa fa-trash" aria-hidden="true" @click="removeCampaign()"></button>
-          </div>
-        </router-link>
-      </li>
-    </ul>
+  <div class="campaigns">
+    <div class="well">
+      <ul>
+        <li v-for="campaign in campaigns">
+          <router-link :to="'/campaigns/'+campaign._id">
+            <div class="well">
+              {{campaign.name}}
+              <button class="fa fa-trash" aria-hidden="true" @click="removeCampaign()"></button>
+            </div>
+          </router-link>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <form @submit.prevent="createCampaign">
+        <input type="text" v-model="name" required placeholder="Campaign Name">
+        <input type="text" v-model="description" required placeholder="Description of Campaign">
+        <button @click="createCampaign">Add Campaign</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -28,8 +36,8 @@
     methods: {
       createCampaign() {
         this.$store.dispatch('createCampaign', {
-          name: 'Testing campaign creation',
-          description: 'blarg'
+          name: 'Kyle Is Tots',
+          description: 'J is da Bomb'
         })
       },
       removeCampaign(campaign) {
@@ -52,15 +60,23 @@
 
   button {
     color: black;
-    margin-bottom: 10px; 
+    margin-bottom: 10px;
   }
-.well{
+
+  .well {
     color: black;
     text-align: left;
     font-family: 'Asap', sans-serif;
     font-weight: bolder;
-}
-.well .well{
-    background-color: #fbfbfb;
-}
+    /*background-color: rgba(100, 100, 100, .5);*/
+    background-color: blue
+  }
+
+  /*.well .well {
+  }*/
+  body {
+background-image: url(../image/mapbackground.jpg);
+background-attachment: fixed;
+background-size: cover;
+  }
 </style>
