b21420adebd3e813520cb40e47f971be2ddc01b1
Samantha Tanner <samz_shadow@yahoo.com>
2017-06-13 16:25:13 -0600

    character template complete...?(v 1.0) ::cat::

--- www/src/components/Character.vue
+++ www/src/components/Character.vue
@@ -1,39 +1,134 @@
 <template>
   <div class="character">
-    <div class="whatever">
+    <div class="row">
     <!--repeat this div for each monster & player, displayed on Encounter.vue-->
-    <div class="col-md-1">Initiative number</div>
-    <div class="col-md-1">player name /monster type</div>
-    <div class="col-md-1">char health</div>
-    <div class="col-md-2">
-      <div class="row">
-        <div class="col-md-2">STR</div>
-        <div class="col-md-2">DEX</div>
-        <div class="col-md-2">CON</div>
-        <div class="col-md-2">WIS</div>
-        <div class="col-md-2">INT</div>
-        <div class="col-md-2">CHA</div>
+      <div class="col-md-1">
+        <div class="row">
+          <div class="col-md-12">
+            <h4>INT</h4>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <input type="number">
+          </div>
+        </div>
       </div>
-      <div class="row">
-        <div class="col-md-2">put value link here</div>
-        <div class="col-md-2">put value link here</div>
-        <div class="col-md-2">put value link here</div>
-        <div class="col-md-2">put value link here</div>
-        <div class="col-md-2">put value link here</div>
-        <div class="col-md-2">put value link here</div>
+      <div class="col-md-2">
+        <div class="row">
+          <div class="col-md-12">
+            <h4>Character Name</h4>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <h4>Character Class</h4>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <h4>Level<input type="number"></h4>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-1">
+        <div class="row">
+          <div class="col-md-12">
+            <h4>HEALTH</h4>
+            <input type="number">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4">
+            <div class="row">
+              <i class="fa fa-angle-up" aria-hidden="true"></i>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="row">
+              <input type="number">
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="row">
+              <i class="fa fa-angle-down" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-1">
+        <div class="row">
+          <div class="col-md-12">
+          <h4>AC</h4><input type="number">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+          <!--SOME SORT OF HOVERABLE ICON THINGY GOES HERE whooohooo!-->
+          </div>
+        </div>
+      </div>
+      <div class="col-md-1">
+        <div class="row">
+          <div class="col-md-12">
+            <h4>SPEED</h4>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+          <input type="number">
+          </div>
+        </div>
+      </div>
+      <div class="col-md-1">
+        <div class="row">
+          <div class="col-md-12">
+            <h4>WEAPONS</h4>
+          </div>
+        </div>
+        <div class="row">
+          <!--SOME SORT OF HOVERABLE ICON THINGY GOES HERE whooohooo!-->
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="row">
+          <!--STATS TITLE-->
+          <div class="col-md-2">STR</div>
+          <div class="col-md-2">DEX</div>
+          <div class="col-md-2">CON</div>
+          <div class="col-md-2">WIS</div>
+          <div class="col-md-2">INT</div>
+          <div class="col-md-2">CHA</div>
+        </div>
+        <div class="row">
+          <!--CHARACTER STATS-->
+          <div class="col-md-2"><input type="number"></div>
+          <div class="col-md-2"><input type="number"></div>
+          <div class="col-md-2"><input type="number"></div>
+          <div class="col-md-2"><input type="number"></div>
+          <div class="col-md-2"><input type="number"></div>
+          <div class="col-md-2"><input type="number"></div>
+        </div>
+        <div class="row">
+          <!--THE STAT MODIFIER-->
+          <div class="col-md-2"><input type="text"></div>
+          <div class="col-md-2"><input type="text"></div>
+          <div class="col-md-2"><input type="text"></div>
+          <div class="col-md-2"><input type="text"></div>
+          <div class="col-md-2"><input type="text"></div>
+          <div class="col-md-2"><input type="text"></div>
+        </div>
+      </div>
+      <div class="col-md-1">
+        <div class="row">
+          <div class="ui checkbox"><input type="checkbox" name="checkbox"></div>
+        </div>
+        <div class="row">
+          <i><img class="tiny" src="http://i44.photobucket.com/albums/f3/suhmantha/skullDelete_zpsmswuacpy.png" @click="removeCharacter"></i><!--THIS FUNCTION NEEDS TO BE WIRED!!!-->
+        </div>
       </div>
     </div>
-    <div class="col-md-2">
-      <div class="col-md-3">armor</div>
-      <div class="col-md-3">initive</div>
-      <div class="col-md-3">speed</div>
-      <div class="col-md-3">info button</div>
-    </div>
-    <div class="col-md-1">
-    <a><img class="tiny" src="http://i44.photobucket.com/albums/f3/suhmantha/skullDelete_zpsmswuacpy.png" @click="removeCharacter"><!--needs to be hooked up--></a>
-    </div>
-  </div>
-
+    
   </div>
 </template>
 
@@ -62,4 +157,3 @@ export default {
     max-width: auto
   }
 </style>
-
