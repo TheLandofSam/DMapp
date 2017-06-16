1d1067e7d23971c891fa0134d5a0d6eb44dcb0d2
Kyle Hyer <krhyer@gmail.com>
2017-06-16 16:14:08 -0600

    gradient looks pretty awesome

--- www/src/App.vue
+++ www/src/App.vue
@@ -22,18 +22,11 @@ export default {
 
 <style>
 body {
-        background-image: url(./image/mapbackground.jpg);
+        background-image: linear-gradient(rgba(0, 0, 0, .6), #3D1600 ), url(./image/mapbackground.jpg);
         background-attachment: fixed;
         background-size: cover;
     } 
-.fullscreen {
-        background: rgba(61, 22, 0, .8);
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-    }   
+
 button {
         margin-top: 10px;
         font-weight: bold;
