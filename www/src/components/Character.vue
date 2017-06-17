2966ed4ffbf7433bb483af1836fe3e6865002cb4
Samantha Tanner <samz_shadow@yahoo.com>
2017-06-16 22:41:00 -0600

    character descp display

--- www/src/components/Character.vue
+++ www/src/components/Character.vue
@@ -22,7 +22,7 @@
         </div>
         <div class="row">
           <div class="col-md-12">
-            <h4>Character Class</h4>
+            <h4>{{character.description}}</h4>
           </div>
         </div>
         <div class="row">
