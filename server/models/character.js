d7ea9cbce757c44639f1b98f30b2ca79f8bdf4fd
Samantha Tanner <samz_shadow@yahoo.com>
2017-06-16 22:31:48 -0600

    another server adjmnt

--- server/models/character.js
+++ server/models/character.js
@@ -5,6 +5,7 @@ let ObjectId = mongoose.Schema.ObjectId
 
 var schema = new mongoose.Schema({
 	name: { type: String, required: true },
+	description: { type: String },
 	created: { type: Number, default: Date.now() },
 	health: {type: Number },
 	maxHealth: {type: Number},
